5b9672caf57d1fb730df4b61d3fe30e54376e446
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-02-13 13:19:11 +0100

    Add database ConnectionString

---
+++ Api+Jwt+UploudImages/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api_Jwt_UploudImages.Data
+{
+   
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+
+            }
+
+
+        }
+    }
+
