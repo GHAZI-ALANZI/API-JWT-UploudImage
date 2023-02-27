c4371754818b3322be5e6ae35b7a6e17d9be50da
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-02-27 12:05:27 +0100

    Custom JWT Auth Extention

--- Api+Jwt+UploudImages/Program.cs
+++ Api+Jwt+UploudImages/Program.cs
@@ -1,7 +1,9 @@
 using Api_Jwt_UploudImages.Data;
 using Api_Jwt_UploudImages.Data.Models;
+using Api_Jwt_UploudImages.Extentions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCustomJwtAuth(builder.Configuration);
 
 var app = builder.Build();
 
